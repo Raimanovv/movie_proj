bc9edc4e216d0cda84312ed88ee517f2650cc56a
rustam <rustam.raimanovv@gmail.com>
2023-10-16 14:24:28 +0500

    0001 - base work

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Movie
+
 
 # Create your views here.
+
+
+def show_all_movies(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies': movies
+    }
+    return render(request, 'movie_app/all_movies.html', context=data)
--- movie_proj/settings.py
+++ movie_proj/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movie_app',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'movie_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
