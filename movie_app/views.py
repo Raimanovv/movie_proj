f55d7a96ba34c2512078510241e0fe04a758e6f9
rustam <rustam.raimanovv@gmail.com>
2023-10-16 20:32:23 +0500

    0002 - DB

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
 # Create your views here.
-
 
 def show_all_movies(request):
     movies = Movie.objects.all()
@@ -11,3 +10,11 @@ def show_all_movies(request):
         'movies': movies
     }
     return render(request, 'movie_app/all_movies.html', context=data)
+
+
+def show_one_movie(request, id_movie: int):
+    movie = get_object_or_404(Movie, id=id_movie)
+    data = {
+        'movie': movie
+    }
+    return render(request, 'movie_app/one_movie.html', context=data)
