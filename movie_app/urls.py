a437c0050c0bce5992aefeefab7186afd1c1c7bd
rustam <rustam.raimanovv@gmail.com>
2023-10-18 18:01:30 +0500

    0003 - add slug

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_all_movies),
-    path('movie/<int:id_movie>', views.show_one_movie, name='movie-detail'),
+    path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -6,14 +6,18 @@ from .models import Movie
 
 def show_all_movies(request):
     movies = Movie.objects.all()
+
+    for movie in movies:
+        movie.save()
+
     data = {
         'movies': movies
     }
     return render(request, 'movie_app/all_movies.html', context=data)
 
 
-def show_one_movie(request, id_movie: int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie: str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     data = {
         'movie': movie
     }
