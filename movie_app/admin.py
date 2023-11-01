d7ee99f7b9fd935cde08dc11676572e948747cda
rustam <rustam.raimanovv@gmail.com>
2023-11-01 20:23:13 +0500

    0011 - add Filter

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -5,6 +5,29 @@ from django.db.models import QuerySet
 
 # Register your models here.
 
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Низкий'),
+            ('от 40 до 59', 'Средний'),
+            ('от 60 до 79', 'Высокий'),
+            ('>=80', 'Высочайший'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        if self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        if self.value() == 'от 60 до 79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=80)
+        if self.value() == '>=80':
+            return queryset.filter(rating__gte=80)
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['name', 'rating', 'currency', 'budget', 'rating_status']
@@ -12,6 +35,8 @@ class MovieAdmin(admin.ModelAdmin):
     ordering = ['-rating', 'name']
     list_per_page = 11
     actions = ['set_dollars', 'set_euro']
+    search_fields = ['name__iregex', 'rating']
+    list_filter = ['name', 'currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, mov: Movie):
@@ -35,4 +60,3 @@ class MovieAdmin(admin.ModelAdmin):
             f'Было обновленно {count_updated} записей',
             messages.ERROR
         )
-
